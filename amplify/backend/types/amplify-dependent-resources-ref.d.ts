3b1dea5e7b31d9367d9f2bbdd7e6e42b893acf51
Brandon <brandon@cupocode.com>
2023-10-31 10:11:28 -0400

    amplify rework

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "dronedriverclientapp": {
+    "dronedriversproduction": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
---
+++ src/ui-components/UsersCreateForm.jsx
@@ -0,0 +1,348 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { Button, Flex, Grid, TextField } from "@aws-amplify/ui-react";
+import { fetchByPath, getOverrideProps, validateField } from "./utils";
+import { API } from "aws-amplify";
+import { createUsers } from "../graphql/mutations";
+export default function UsersCreateForm(props) {
+  const {
+    clearOnSuccess = true,
+    onSuccess,
+    onError,
+    onSubmit,
+    onValidate,
+    onChange,
+    overrides,
+    ...rest
+  } = props;
+  const initialValues = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    registrationDate: "",
+    purchased: "",
+    untitledfield: "",
+  };
+  const [email, setEmail] = React.useState(initialValues.email);
+  const [firstName, setFirstName] = React.useState(initialValues.firstName);
+  const [lastName, setLastName] = React.useState(initialValues.lastName);
+  const [registrationDate, setRegistrationDate] = React.useState(
+    initialValues.registrationDate
+  );
+  const [purchased, setPurchased] = React.useState(initialValues.purchased);
+  const [untitledfield, setUntitledfield] = React.useState(
+    initialValues.untitledfield
+  );
+  const [errors, setErrors] = React.useState({});
+  const resetStateValues = () => {
+    setEmail(initialValues.email);
+    setFirstName(initialValues.firstName);
+    setLastName(initialValues.lastName);
+    setRegistrationDate(initialValues.registrationDate);
+    setPurchased(initialValues.purchased);
+    setUntitledfield(initialValues.untitledfield);
+    setErrors({});
+  };
+  const validations = {
+    email: [],
+    firstName: [],
+    lastName: [],
+    registrationDate: [],
+    purchased: [],
+    untitledfield: [],
+  };
+  const runValidationTasks = async (
+    fieldName,
+    currentValue,
+    getDisplayValue
+  ) => {
+    const value =
+      currentValue && getDisplayValue
+        ? getDisplayValue(currentValue)
+        : currentValue;
+    let validationResponse = validateField(value, validations[fieldName]);
+    const customValidator = fetchByPath(onValidate, fieldName);
+    if (customValidator) {
+      validationResponse = await customValidator(value, validationResponse);
+    }
+    setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }));
+    return validationResponse;
+  };
+  return (
+    <Grid
+      as="form"
+      rowGap="15px"
+      columnGap="15px"
+      padding="20px"
+      onSubmit={async (event) => {
+        event.preventDefault();
+        let modelFields = {
+          email,
+          firstName,
+          lastName,
+          registrationDate,
+          purchased,
+          untitledfield,
+        };
+        const validationResponses = await Promise.all(
+          Object.keys(validations).reduce((promises, fieldName) => {
+            if (Array.isArray(modelFields[fieldName])) {
+              promises.push(
+                ...modelFields[fieldName].map((item) =>
+                  runValidationTasks(fieldName, item)
+                )
+              );
+              return promises;
+            }
+            promises.push(
+              runValidationTasks(fieldName, modelFields[fieldName])
+            );
+            return promises;
+          }, [])
+        );
+        if (validationResponses.some((r) => r.hasError)) {
+          return;
+        }
+        if (onSubmit) {
+          modelFields = onSubmit(modelFields);
+        }
+        try {
+          Object.entries(modelFields).forEach(([key, value]) => {
+            if (typeof value === "string" && value === "") {
+              modelFields[key] = null;
+            }
+          });
+          await API.graphql({
+            query: createUsers.replaceAll("__typename", ""),
+            variables: {
+              input: {
+                ...modelFields,
+              },
+            },
+          });
+          if (onSuccess) {
+            onSuccess(modelFields);
+          }
+          if (clearOnSuccess) {
+            resetStateValues();
+          }
+        } catch (err) {
+          if (onError) {
+            const messages = err.errors.map((e) => e.message).join("\n");
+            onError(modelFields, messages);
+          }
+        }
+      }}
+      {...getOverrideProps(overrides, "UsersCreateForm")}
+      {...rest}
+    >
+      <TextField
+        label="Email"
+        isRequired={false}
+        isReadOnly={false}
+        value={email}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email: value,
+              firstName,
+              lastName,
+              registrationDate,
+              purchased,
+              untitledfield,
+            };
+            const result = onChange(modelFields);
+            value = result?.email ?? value;
+          }
+          if (errors.email?.hasError) {
+            runValidationTasks("email", value);
+          }
+          setEmail(value);
+        }}
+        onBlur={() => runValidationTasks("email", email)}
+        errorMessage={errors.email?.errorMessage}
+        hasError={errors.email?.hasError}
+        {...getOverrideProps(overrides, "email")}
+      ></TextField>
+      <TextField
+        label="First name"
+        isRequired={false}
+        isReadOnly={false}
+        value={firstName}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              firstName: value,
+              lastName,
+              registrationDate,
+              purchased,
+              untitledfield,
+            };
+            const result = onChange(modelFields);
+            value = result?.firstName ?? value;
+          }
+          if (errors.firstName?.hasError) {
+            runValidationTasks("firstName", value);
+          }
+          setFirstName(value);
+        }}
+        onBlur={() => runValidationTasks("firstName", firstName)}
+        errorMessage={errors.firstName?.errorMessage}
+        hasError={errors.firstName?.hasError}
+        {...getOverrideProps(overrides, "firstName")}
+      ></TextField>
+      <TextField
+        label="Last name"
+        isRequired={false}
+        isReadOnly={false}
+        value={lastName}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              firstName,
+              lastName: value,
+              registrationDate,
+              purchased,
+              untitledfield,
+            };
+            const result = onChange(modelFields);
+            value = result?.lastName ?? value;
+          }
+          if (errors.lastName?.hasError) {
+            runValidationTasks("lastName", value);
+          }
+          setLastName(value);
+        }}
+        onBlur={() => runValidationTasks("lastName", lastName)}
+        errorMessage={errors.lastName?.errorMessage}
+        hasError={errors.lastName?.hasError}
+        {...getOverrideProps(overrides, "lastName")}
+      ></TextField>
+      <TextField
+        label="Registration date"
+        isRequired={false}
+        isReadOnly={false}
+        type="date"
+        value={registrationDate}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              firstName,
+              lastName,
+              registrationDate: value,
+              purchased,
+              untitledfield,
+            };
+            const result = onChange(modelFields);
+            value = result?.registrationDate ?? value;
+          }
+          if (errors.registrationDate?.hasError) {
+            runValidationTasks("registrationDate", value);
+          }
+          setRegistrationDate(value);
+        }}
+        onBlur={() => runValidationTasks("registrationDate", registrationDate)}
+        errorMessage={errors.registrationDate?.errorMessage}
+        hasError={errors.registrationDate?.hasError}
+        {...getOverrideProps(overrides, "registrationDate")}
+      ></TextField>
+      <TextField
+        label="Purchased"
+        isRequired={false}
+        isReadOnly={false}
+        value={purchased}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              firstName,
+              lastName,
+              registrationDate,
+              purchased: value,
+              untitledfield,
+            };
+            const result = onChange(modelFields);
+            value = result?.purchased ?? value;
+          }
+          if (errors.purchased?.hasError) {
+            runValidationTasks("purchased", value);
+          }
+          setPurchased(value);
+        }}
+        onBlur={() => runValidationTasks("purchased", purchased)}
+        errorMessage={errors.purchased?.errorMessage}
+        hasError={errors.purchased?.hasError}
+        {...getOverrideProps(overrides, "purchased")}
+      ></TextField>
+      <TextField
+        label="Untitledfield"
+        isRequired={false}
+        isReadOnly={false}
+        value={untitledfield}
+        onChange={(e) => {
+          let { value } = e.target;
+          if (onChange) {
+            const modelFields = {
+              email,
+              firstName,
+              lastName,
+              registrationDate,
+              purchased,
+              untitledfield: value,
+            };
+            const result = onChange(modelFields);
+            value = result?.untitledfield ?? value;
+          }
+          if (errors.untitledfield?.hasError) {
+            runValidationTasks("untitledfield", value);
+          }
+          setUntitledfield(value);
+        }}
+        onBlur={() => runValidationTasks("untitledfield", untitledfield)}
+        errorMessage={errors.untitledfield?.errorMessage}
+        hasError={errors.untitledfield?.hasError}
+        {...getOverrideProps(overrides, "untitledfield")}
+      ></TextField>
+      <Flex
+        justifyContent="space-between"
+        {...getOverrideProps(overrides, "CTAFlex")}
+      >
+        <Button
+          children="Clear"
+          type="reset"
+          onClick={(event) => {
+            event.preventDefault();
+            resetStateValues();
+          }}
+          {...getOverrideProps(overrides, "ClearButton")}
+        ></Button>
+        <Flex
+          gap="15px"
+          {...getOverrideProps(overrides, "RightAlignCTASubFlex")}
+        >
+          <Button
+            children="Submit"
+            type="submit"
+            variation="primary"
+            isDisabled={Object.values(errors).some((e) => e?.hasError)}
+            {...getOverrideProps(overrides, "SubmitButton")}
+          ></Button>
+        </Flex>
+      </Flex>
+    </Grid>
+  );
+}
