1c4e0a8d7053eccec68653fe120776ca978d3592
Brandon <brandon@cupocode.com>
2023-12-11 21:13:49 -0500

    fixes to lambda function

--- amplify/backend/function/usersDroneDriverTrigger46960f76/src/index.js
+++ amplify/backend/function/usersDroneDriverTrigger46960f76/src/index.js
@@ -1,6 +1,25 @@
 const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    console.log('AWS SDK version:', AWS.VERSION);
+    let date = new Date();
+
+    const params = {
+        TableName: DroneDriversUserTable,
+        Item: {
+            'id': event.request.userAttributes.sub,
+            'email': event.request.userAttributes.email,
+            'hasPaid': false,
+            // other attributes...
+        }
+    };
+
+    try {
+        await ddb.put(params).promise();
+        console.log("User added to DynamoDB");
+    } catch (err) {
+        console.log("Error adding user to DynamoDB", err);
+    }
+
     return event;
 };
