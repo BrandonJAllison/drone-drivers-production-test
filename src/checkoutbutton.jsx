b554a43ddeef4e5c3fe8580cb75115d60daa58a3
Brandon <brandon@cupocode.com>
2024-02-04 21:32:53 -0500

    stripe

--- src/checkoutbutton.jsx
+++ src/checkoutbutton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { API } from 'aws-amplify';
 import { loadStripe } from '@stripe/stripe-js';
 
 // Replace with your Stripe public key
@@ -10,22 +11,10 @@ const CheckoutButton = () => {
     const stripe = await stripePromise;
 
     try {
-      // Call your endpoint directly to create the Checkout session
-      const response = await fetch('https://s5q926428f.execute-api.us-east-1.amazonaws.com/master/stripecheckout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          // Include any other headers your API requires
-        },
-        body: JSON.stringify({}), // any required body content
+      // Call your Amplify backend to create the Checkout session
+      const session = await API.post('stripecheckoutapi', '/stripecheckout', {
+        body: {}, // any required body content
       });
-
-      const session = await response.json();
-
-      // Check if the request was successful
-      if (!response.ok) {
-        throw new Error(session.message || 'Failed to create checkout session');
-      }
 
       // Use Stripe's 'redirectToCheckout' method to redirect to Stripe's Checkout page
       const result = await stripe.redirectToCheckout({
