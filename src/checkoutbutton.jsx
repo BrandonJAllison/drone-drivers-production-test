adf8671065ed85000bfe03f7138ef27931d1d1c5
Brandon <brandon@cupocode.com>
2024-02-11 15:52:44 -0500

    fix for react env variable for stripe

--- src/checkoutbutton.jsx
+++ src/checkoutbutton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
 // Replace with your Stripe public key
-const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const CheckoutButton = () => {
   const handleClick = async (event) => {
