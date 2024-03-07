c10d42349a4fb618fe1448d9eaf81b8aa03a6ecf
Brandon <brandon@cupocode.com>
2024-03-07 14:03:49 -0500

    hasPaid updated

--- src/CourseAccessContext.js
+++ src/CourseAccessContext.js
@@ -12,8 +12,8 @@ export const CourseAccessProvider = ({ user, children }) => {
         try {
             const response = await fetch(`https://plankton-app-3pnzq.ondigitalocean.app/api/user/${user.username}`);
             const data = await response.json();
-            console.log('has paid', data); // Adjusted to match the backend property name
-            setHasPaid(data.has_paid);
+            console.log('has paid', data[0].has_paid); // Access has_paid from the first item in the data array
+            setHasPaid(data[0].has_paid);
         } catch (error) {
             console.error("Error fetching user payment status at this time :", error);
         }
