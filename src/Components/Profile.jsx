20cb1f93afe953e7b3f654982072fe34894fec74
Brandon <brandon@cupocode.com>
2024-03-11 19:04:24 -0400

    updating for loading

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Card, CardContent, Typography, CardMedia, Grid, List, ListItem, ListItemText } from '@mui/material';
+import { Box, Card, CardContent, Typography, CardMedia, Grid, List, ListItem, ListItemText, CircularProgress } from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import CheckoutButton from '../checkoutbutton.jsx';
 import { useCourseAccess } from '../CourseAccessContext.js';
 
 const Profile = ({ user, signout }) => {
     const { hasPaid } = useCourseAccess();
-    const userID = user.attributes.sub
+    const [isLoading, setIsLoading] = useState(true); // Initialize loading state to true
     const navigate = useNavigate();
     
+    useEffect(() => {
+        if (user && user.attributes && user.attributes.sub) {
+            setIsLoading(false); // Set loading to false once user data is confirmed to be loaded
+        }
+    }, [user]); // Depend on user to re-check when it changes
 
-    // Function to handle the click on the purchase button
     const handlePurchaseClick = async () => {
         if (!user || !user.attributes.sub) {
             console.error('UserID is not available.');
             return;
         }
-        const userID = user.attributes.sub; // Ensuring userID is taken directly within the function
+        const userID = user.attributes.sub;
         console.log('Attempting to initiate checkout for:', userID);
     
         try {
@@ -33,86 +37,79 @@ const Profile = ({ user, signout }) => {
     };
 
     async function initiateCheckout(userID) {
-        
-        console.log('Initiating checkout for:', userID); // Logging the username for debugging
-        console.log('Sending payload:', userID);
-        
+        console.log('Initiating checkout for:', userID);
         const response = await fetch('https://plankton-app-3pnzq.ondigitalocean.app/api/create-checkout-session', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-         
-                userID: userID, // Include your actual userID for reference
-                
-            })
+            body: JSON.stringify({ userID })
         });
     
         if (!response.ok) {
-
             throw new Error('Network response was not ok');
         }
     
-        return await response.json(); // Assuming this returns { url: "stripe_checkout_url" }
+        return await response.json();
     }
 
+    if (isLoading) {
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <CircularProgress />
+            </Box>
+        );
+    }
 
     return (
         <Box sx={{ flexGrow: 1, p: 3 }}>
-            {user && (
-                <>
-                    <Grid container spacing={2}>
-                        {/* User Information */}
-                        <Grid item xs={12}>
-                            <Card raised>
-                                <CardContent>
-                                    <Typography variant="h5" component="div">User Information For:</Typography>
-                                    <p>{userID}</p>
-                                    <List dense>
-                                        <ListItem><ListItemText primary="Name" secondary={user.attributes.name} /></ListItem>
-                                        <ListItem><ListItemText primary="Username" secondary={user.attributes.preferred_username} /></ListItem>
-                                        <ListItem><ListItemText primary="Email" secondary={user.attributes.email} /></ListItem>
-                                    </List>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+            <Grid container spacing={2}>
+                {/* User Information */}
+                <Grid item xs={12}>
+                    <Card raised>
+                        <CardContent>
+                            <Typography variant="h5" component="div">User Information</Typography>
+                            <List dense>
+                                <ListItem><ListItemText primary="Name" secondary={user.attributes.name} /></ListItem>
+                                <ListItem><ListItemText primary="Username" secondary={user.attributes.preferred_username} /></ListItem>
+                                <ListItem><ListItemText primary="Email" secondary={user.attributes.email} /></ListItem>
+                            </List>
+                        </CardContent>
+                    </Card>
+                </Grid>
 
-                        {/* Courses */}
-                        <Grid item xs={12}>
-                            <Card raised>
-                                <CardContent>
-                                    <Typography variant="h5" component="div">
-                                        Courses
-                                    </Typography>
-                                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pt: 2 }}>
-                                        <Box sx={{ position: 'relative', display: 'inline-block' }}>
-                                            <CardMedia
-                                                component="img"
-                                                sx={{ width: 151, filter: !hasPaid ? 'grayscale(100%)' : 'none' }}
-                                                image="https://dronedriver.com/wp-content/uploads/2023/11/part-107-card.png"
-                                                alt="FAA Part 107 Training Course"
-                                            />
-                                            {!hasPaid && (
-                                                <LockIcon 
-                                                    color="error" 
-                                                    sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} 
-                                                />
-                                            )}
-                                        </Box>
-                                        {user && !hasPaid && (
-                                            <Box sx={{ mt: 1 }}>
-                                                {/* Ensure `onClick` uses an arrow function to correctly capture the current `userID` */}
-                                                <CheckoutButton onClick={handlePurchaseClick} />
-                                            </Box>
-                                        )}
+                {/* Courses */}
+                <Grid item xs={12}>
+                    <Card raised>
+                        <CardContent>
+                            <Typography variant="h5" component="div">Courses</Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', pt: 2 }}>
+                                <Box sx={{ position: 'relative', display: 'inline-block' }}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 151, filter: !hasPaid ? 'grayscale(100%)' : 'none' }}
+                                        image="https://dronedriver.com/wp-content/uploads/2023/11/part-107-card.png"
+                                        alt="FAA Part 107 Training Course"
+                                    />
+                                    {!hasPaid && (
+                                        <LockIcon 
+                                            color="error" 
+                                            sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} 
+                                        />
+                                    )}
+                                </Box>
+                                {!hasPaid && (
+                                    <Box sx={{ mt: 1 }}>
+                                        <CheckoutButton onClick={handlePurchaseClick} />
                                     </Box>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+                                )}
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Grid>
 
-                        {/* Test Scores */}
-                        <Grid item xs={12}>
+                {/* Test Scores */}
+                <Grid item xs={12}>
                             <Card raised>
                                 <CardContent>
                                     <Typography variant="h5" component="div">Test Scores</Typography>
@@ -136,11 +133,9 @@ const Profile = ({ user, signout }) => {
                                 </CardContent>
                             </Card>
                         </Grid>
-                    </Grid>
-                </>
-            )}
+            </Grid>
         </Box>
     );
-}
+};
 
 export default Profile;
